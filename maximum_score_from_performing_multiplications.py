f81f2d87b74fd33e87a9a6387e482a6d6e873b6b
gamarante <gusamarante@gmail.com>
2022-07-19 12:25:22 -0200

    added new problem

--- maximum_score_from_performing_multiplications.py
+++ maximum_score_from_performing_multiplications.py
@@ -24,14 +24,18 @@ def max_score(nums, multipliers):
 
     n, m = len(nums), len(multipliers)
 
+    memo = {}
+
     def dp(i, left):
         if i == m:
             return 0
 
-        mult = multipliers[i]
-        right = n - 1 - (i - left)
+        if (i, left) not in memo:
+            mult = multipliers[i]
+            right = n - 1 - (i - left)
+            memo[(i, left)] = max(mult * nums[left] + dp(i + 1, left + 1), mult * nums[right] + dp(i + 1, left))
 
-        return max(mult * nums[left] + dp(i + 1, left + 1), mult * nums[right] + dp(i + 1, left))
+        return memo[(i, left)]
 
     return dp(0, 0)
 
